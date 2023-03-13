904ae6c11f7748e77700fdbfa2a848a4e5e75625
iqbalmaulanaakbar <iqbalmaulanaakbar21@gmail.com>
2023-03-13 10:16:02 +0700

    mengupdate tampilan footer user, footer admin, memperbaiki foto berita yg tidak muncul, mengupdate css

--- application/views/user/footer.php
+++ application/views/user/footer.php
@@ -1,17 +1,69 @@
+<footer>
 
-    <footer>
-    <div class="copyright">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 text-center">
-            <br>
-
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;<script>document.write(new Date().getFullYear());</script> Sanggar Tari Mimi Rasinah. All rights reserved.</a>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+  <div class="container pt-5">
+    <div class="row">
+      <div class="col-12">
+        <div class="row" style="margin-top: 25px;">
+          <div class="col col-md-4 col-12">
+            <h4 class="font-weight-normal text-white">Sanggar Gondewarasa</h4>
+          </div>
+          <div class="col col-md-4 col-12">
+            <h4 class="font-weight-normal text-white">Info Sanggar Gondewarasa</h4>
+          </div>
+          <div class="col col-md-4 col-12">
+            <h4 class="font-weight-normal text-white">Kunjungi Kami</h4>
           </div>
         </div>
       </div>
     </div>
-    </footer>
-    
+    <div class="row">
+      <div class="col-12">
+        <div class="row" style="margin-top: 35px;">
+          <div class="col col-md-4 col-12">
+            <ul class="items-footer list-unstyled mb-0">
+              <p class="text-white">Jl. Arief Rahman, Kotakaler, Kec. Sumedang Utara, Kabupaten Sumedang, Jawa Barat 45621</p>
+              <!-- <li><a href="#" class="text-white">Tracking</a></li>
+              <li><a href="#" class="text-white">Shipping</a></li>
+              <li><a href="#" class="text-white">Help & Support</a></li>
+              <li><a href="#" class="text-white">Communication Preference</a></li> -->
+            </ul>
+          </div>
+          <div class="col col-md-4 col-12">
+            <ul class="items-footer list-unstyled mb-0">
+              <li><a href="<?php echo site_url() ?>berita" class="text-white">Berita<i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
+              <li><a href="<?php echo site_url() ?>profil" class="text-white">Profil <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
+              <li><a href="<?php echo site_url() ?>produk" class="text-white">Produk <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
+              <li><a href="<?php echo site_url() ?>jasa" class="text-white">Jasa <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
+            </ul>
+          </div>
+          <div class="col col-md-4 col-12">
+            <ul class="items-footer list-unstyled mb-0">
+              <li><a href="#" class="text-white"><i class="fa-brands fa-facebook"></i> Facebook <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
+              <li><a href="#" class="text-white"><i class="fa-brands fa-twitter"></i> Twitter <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
+              <li><a href="#" class="text-white"><i class="fa-brands fa-instagram"></i> Instagram <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
+              <li><a href="#" class="text-white"><i class="fa-brands fa-youtube"></i>Youtube <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <div class="copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <br>
+
+          <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>
+              document.write(new Date().getFullYear());
+            </script> Sanggar Gondewarasa. All rights reserved.</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,12 +1,12 @@
-    <section class="home-slider owl-carousel img" style="height: 37rem">
+    <section class="home-slider owl-carousel img" style="height: 50rem">
 
       <div class="slider-item" style="background-image: url(<?php echo base_url(); ?>user/slider/kuning.jpg);">
-      	<div class="overlay"></div>
+        <div class="overlay"></div>
         <div class="container">
           <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
 
             <div class="col-md-7 col-sm-12 text-center ftco-animate">
-            	<span class="subheading">Sangar Gondewarasa</span>
+              <span class="subheading">Sangar Gondewarasa</span>
               <p class="mb-4 mb-md-5">Sanggar Tari Tradisional Sunda</p>
             </div>
 
@@ -15,12 +15,12 @@
       </div>
 
       <div class="slider-item" style="background-image: url(<?php echo base_url(); ?>user/slider/6.jpg);">
-      	<div class="overlay"></div>
+        <div class="overlay"></div>
         <div class="container">
           <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
 
             <div class="col-md-9 col-sm-15 text-center ftco-animate">
-            	<span class="subheading">Kelas Tari</span>
+              <span class="subheading">Kelas Tari</span>
               <p class="mb-4 mb-md-5">lomba tari Gondewarasa</p>
             </div>
 
@@ -29,12 +29,12 @@
       </div>
 
       <div class="slider-item" style="background-image: url(<?php echo base_url(); ?>user/slider/8.jpg);">
-      	<div class="overlay"></div>
+        <div class="overlay"></div>
         <div class="container">
           <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
 
             <div class="col-md-7 col-sm-12 text-center ftco-animate">
-            	<span class="subheading">Oky Wulandari,S.sn</span>
+              <span class="subheading">Oky Wulandari,S.sn</span>
               <p class="mb-4 mb-md-5">Pendiri Sanggar Gondewarasa</p>
             </div>
 
@@ -44,7 +44,7 @@
     </section>
 
     <section class="ftco-section" style="background-color: whitesmoke;">
-      <div class="container" >
+      <div class="container">
         <div class="row justify-content-center mb-5 pb-3">
           <div class="col-md-7 heading-section ftco-animate text-center">
             <h2 class="mb-4" style="color: black"><b>Berita</b></h2>
@@ -52,80 +52,81 @@
           </div>
         </div>
         <div class="row d-flex">
-        <?php
-          $hasil3= $this->db->query("SELECT * from agenda");
-          foreach($hasil3->result() as $agenda){
-        ?>
-          <div class="col-md-4 d-flex ftco-animate">
-          	<div class="blog-entry align-self-stretch">
-              <a href="blog-single.html" class="block-20" style="background-image: url('<?php echo base_url(); ?>user/agenda/<?php echo $agenda->foto_agenda ?>');">
-              </a>
-              <div class="text py-4 d-block">
-              	<div class="meta">
-                  <div><a><span class="icon-calendar"> <?php echo $agenda->tgl_agenda ?></span> | <?php echo $agenda->waktu_agenda ?> </a></div>
-                  <div><a><?php echo $agenda->tempat_agenda ?></a></div>
+          <?php
+          $hasil3 = $this->db->query("SELECT * from agenda");
+          foreach ($hasil3->result() as $agenda) {
+          ?>
+            <div class="col-md-4 d-flex ftco-animate">
+              <div class="blog-entry align-self-stretch">
+                <a href="blog-single.html" class="block-20" style="background-image: url('<?php echo base_url(); ?>user/agenda/<?php echo $agenda->foto_agenda ?>');">
+                </a>
+                <div class="text py-4 d-block">
+                  <div class="meta">
+                    <div><a><span class="icon-calendar"> <?php echo $agenda->tgl_agenda ?></span> | <?php echo $agenda->waktu_agenda ?> </a></div>
+                    <div><a><?php echo $agenda->tempat_agenda ?></a></div>
+                  </div>
+                  <h3 class="heading mt-2"><a href="<?php echo base_url(); ?>berita/" style="color: black"><?php echo $agenda->judul_agenda ?></a></h3>
+                  <!-- <p><?php // echo $agenda->isi_agenda 
+                          ?></p> -->
                 </div>
-                <h3 class="heading mt-2" ><a href="<?php echo base_url(); ?>berita/" style="color: black"><?php echo $agenda->judul_agenda ?></a></h3>
-                <!-- <p><?php // echo $agenda->isi_agenda ?></p> -->
               </div>
             </div>
-          </div>
-          <?php 
-             } 
+          <?php
+          }
           ?>
         </div>
       </div>
       <section class="ftco-gallery">
-    	<div class="container-wrap">
-        <div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4" style="color: black"><b>Galeri Foto</b></h2>
-            <p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p>
-          </div>
-        </div>
-    		<div class="row no-gutters">
-        <?php
-          $hasil= $this->db->query("SELECT * from foto");
-          foreach($hasil->result() as $foto){
-        ?>
-					<div class="col-md-3 ftco-animate">
-						<a href="gallery.html" class="gallery img d-flex align-items-center" style="background-image: url(<?php echo base_url(); ?>user/galeri/<?php echo $foto->foto_galeri; ?>);">
-							<div class="icon mb-4 d-flex align-items-center justify-content-center">
-    						<span class="icon-search"></span>
-    					</div>
-						</a>
-					</div>
-					<?php 
-             } 
-          ?>
-                </div>
+        <div class="container-wrap">
+          <div class="row justify-content-center mb-5 pb-3">
+            <div class="col-md-7 heading-section ftco-animate text-center">
+              <h2 class="mb-4" style="color: black"><b>Galeri Foto</b></h2>
+              <p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p>
             </div>
-    </section>
-    <section class="ftco-gallery">
-    	<div class="container-wrap">
-        <div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4" style="color: black"><b>Galeri Video</b></h2>
-            <p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p>
+          </div>
+          <div class="row no-gutters">
+            <?php
+            $hasil = $this->db->query("SELECT * from foto");
+            foreach ($hasil->result() as $foto) {
+            ?>
+              <div class="col-md-3 ftco-animate">
+                <a href="gallery.html" class="gallery img d-flex align-items-center" style="background-image: url(<?php echo base_url(); ?>user/galeri/<?php echo $foto->foto_galeri; ?>);">
+                  <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                    <span class="icon-search"></span>
+                  </div>
+                </a>
+              </div>
+            <?php
+            }
+            ?>
           </div>
         </div>
-    		<div class="row no-gutters">
-        <?php
-          $hasil= $this->db->query("SELECT * from video");
-          foreach($hasil->result() as $video){
-        ?>
-					<div class="col-md-3 ftco-animate">
-          <iframe src="<?php echo $video->link_video?>" frameborder="0" allowfullscreen></iframe>
-						<!-- <a href="gallery.html" class="gallery img d-flex align-items-center" style="background-image: url(<?php echo base_url(); ?>user/galeri/<?php echo $foto->foto_galeri; ?>);"> -->
-							<!-- <div class="icon mb-4 d-flex align-items-center justify-content-center">
+      </section>
+      <section class="ftco-gallery">
+        <div class="container-wrap">
+          <div class="row justify-content-center mb-5 pb-3">
+            <div class="col-md-7 heading-section ftco-animate text-center">
+              <h2 class="mb-4" style="color: black"><b>Galeri Video</b></h2>
+              <p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p>
+            </div>
+          </div>
+          <div class="row no-gutters">
+            <?php
+            $hasil = $this->db->query("SELECT * from video");
+            foreach ($hasil->result() as $video) {
+            ?>
+              <div class="col-md-3 ftco-animate">
+                <iframe src="<?php echo $video->link_video ?>" frameborder="0" allowfullscreen></iframe>
+                <!-- <a href="gallery.html" class="gallery img d-flex align-items-center" style="background-image: url(<?php echo base_url(); ?>user/galeri/<?php echo $foto->foto_galeri; ?>);"> -->
+                <!-- <div class="icon mb-4 d-flex align-items-center justify-content-center">
     						<span class="icon-search"></span>
               </div> -->
-						</a>
-					</div>
-					<?php 
-             } 
-          ?>
-                </div>
-            </div>
-    </section>
+                </a>
+              </div>
+            <?php
+            }
+            ?>
+          </div>
+        </div>
+      </section>
     </section>
