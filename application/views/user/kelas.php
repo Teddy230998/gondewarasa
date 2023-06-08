6fc3056450e87c19e0d2f3ae29d0168b3b83ad5a
teddy <teddyseptian000@gmail.com>
2023-06-08 23:13:36 +0700

    kelas tari yang sudah di rubah

--- application/views/user/kelas.php
+++ application/views/user/kelas.php
@@ -1,31 +1,26 @@
 <section class="ftco-section" style="background-color: whitesmoke;">
     <div class="container">
         <div class="row justify-content-center mb-5 pb-3">
-            <div class="col-md-7 heading-section ftco-animate text-center">
-                <h2 class="mb-4" style="color: black"><b>Kelas Tari</b></h2>
-                <p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p>
-
-            </div>
+            <div class="col-md-12 heading-section ftco-animate text-center d-flex justify-content-center flex-column align-items-center">
+                <!-- <h2 class="mb-4" style="color: black"><b>Kelas Tari</b></h2>
+                <p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p> -->
+                <h1 class="text-dark">Kelas Tari</h1>
+		<img class="mb-3" src="<?= base_url()?>user/logo/sanggar.jpg" alt="Banner" width="800" height="200">
+      <p class="text-dark text-center mb-2" style="max-width: 720px;">Sanggar Gondewarasa menyediakan berbagai macam kelas tari khususnya di tari tradisi sunda, kelas tari sanggar gondewarasa menyediakan kelas privata dan kelas leguler, bagi yang ingin mendaftar tari silahkan kunjungi link di bawah :</p>
+    <p class="text-dark">link: </p>
+    <a href="https://yandex.com/" target="_blank">Pendaftaran</a>        
+    </div>
         </div>
-        <div class="row">
-            <?php
-            $hasil = $this->db->query("SELECT * from produk");
-            foreach ($hasil->result() as $produk) {
-            ?>
-                <div class="col-md-3 text-center ftco-animate">
-                    <div class="menu-wrap">
-                        <a href="#" class="menu-img img mb-4" style="background-image: url(<?php echo base_url(); ?>user/produk_dan_jasa/<?php echo $produk->foto; ?>);"></a>
-                        <div class="text">
-                            <h3><a href="#" style="color: black"><?php echo $produk->judul ?></a></h3>
-                            <p>Stok : <?php echo $produk->stok ?></p>
-                            <p class="price"><span><?php echo 'Rp. ' . number_format($produk->harga) ?></span></p>
-                            <p><a href="<?php echo site_url() ?>login" class="btn btn-white btn-outline-black" style="color: gray">Daftar</a></p>
-                        </div>
-                    </div>
-                </div>
-            <?php
-            }
-            ?>
+        <div class="one-half img">
+      </div>
+      
+      <!-- <div class="one-half ftco-animate">
+        <div class="heading-section ftco-animate ">
+          <h2 class="mb-4" style="color: black"><b> Kelas Tari</b> </h2>
         </div>
+        <div>
+  				<p>Kelas Tari Adalah</p>
+  			</div>
+      </div> -->
     </div>
 </section>
